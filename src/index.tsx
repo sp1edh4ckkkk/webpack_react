4937953a822962c2e952e0c583bb26098500a477
sp1edh4ckkkk <sp11dh4ckk@gmail.com>
2025-04-22 10:02:15 +0300

    update

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import style from './index.module.scss';
+import './index.scss';
+import './assets/images/dog.jpg';
+
+console.log(style); //использование объекта style важно для попадания стилей в css-бандл
